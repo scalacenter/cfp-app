e6ad37964dd42fb8aa6bc30401bde82149d9db77
Jeneve Parrish <jeneve.parrish@gmail.com>
2017-11-01 15:58:15 -0600

    Fixed syntax error

--- app/assets/javascripts/staff/program/sessions.js
+++ app/assets/javascripts/staff/program/sessions.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
         filterProgramSessionsBy("program")
 
         $(".quick-filter-tabs .tab").on('click', function(e) {
-            filterProgramSessionsBy(e.currentTarget.id))
-        }
+            filterProgramSessionsBy(e.currentTarget.id)
+        })
     }
 })
 
