ee5a1e8e93f6d15236a9b3b6c611e33254b5578a
Julien Richard-Foy <julien@richard-foy.fr>
2017-11-28 15:40:20 +0100

    Make webpage optional

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -11,7 +11,7 @@ class Speaker < ApplicationRecord
 
   validates :event, presence: true
   validates :bio, length: {maximum: 500}
-  validates :name, :email, :webpage, presence: true, unless: :skip_name_email_validation
+  validates :name, :email, presence: true, unless: :skip_name_email_validation
   validates_format_of :email, with: Devise.email_regexp
 
   attr_accessor :skip_name_email_validation
